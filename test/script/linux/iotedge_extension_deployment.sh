fb37a8830590ee06c1b70bfb8d9aaa36f5d79d20
v-cheye <v-cheye@microsoft.com>
2018-12-28 09:50:05 +0800

    update

--- test/script/linux/iotedge_extension_deployment.sh
+++ test/script/linux/iotedge_extension_deployment.sh
@@ -1,7 +1,7 @@
 # Install Nodejs
-sudo apt-get install curl
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install curl -y
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install nodejs -y
 
 # Install yo
 sudo npm install -g yo
@@ -10,10 +10,10 @@ sudo npm install -g yo
 sudo npm i -g yo generator-azure-iot-edge-module
 
 # Install git
-sudo apt-get install git
+sudo apt-get install git -y
 
 # Install vs code
-sudo apt-get install software-properties-common apt-transport-https wget
+sudo apt-get install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt-get install code -y
@@ -22,7 +22,7 @@ sudo apt-get install code -y
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo add-apt-repository universe
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https -y
 sudo apt-get update
 sudo apt-get install dotnet-sdk-2.2 -y
 
@@ -40,7 +40,7 @@ pip install iotedgehubdev
 
 # Install docker
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
